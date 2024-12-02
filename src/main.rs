6c04e7d0c1f24a57c98e9d41b93c2041e1069782
wazabanda <1wazabanda@gmail.com>
2024-12-02 08:52:47 +0200

    day 2 part 2

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,11 @@ use std::io::{self,BufRead};
 
 fn main() -> io::Result<()> {
     
-    let path = "src/inputs/day1.txt";
+    let path = "src/inputs/day2.txt";
     let file = File::open(path)?;
     let reader = io::BufReader::new(file);
 
-    let mut left_numbers = vec![];
-    let mut right_numbers = vec![];
+    let mut safe = 0;
 
     for line in reader.lines(){
         let line = line?;
@@ -18,20 +17,61 @@ fn main() -> io::Result<()> {
             .filter_map(|s| s.parse::<i32>().ok())
             .collect();
 
-        left_numbers.push(numbers[0]);
-        right_numbers.push(numbers[1]);
+        let mut prev = 0;
+        prev = numbers[0];
+        let mut is_safe = true;
+        let mut incr = true;
+        let mut unsafe_count = 0;
+        for i in 1..numbers.len(){
+            let num = numbers[i];
+            
+            if i == 1 || unsafe_count == 1{
+                incr = prev < num;
+            }
+
+            if (incr && prev > num) || (incr == false && prev < num){
+                
+                is_safe = false;
+                unsafe_count += 1;
+                //prev = num;
+                if unsafe_count > 1{
+                    break;
+                }
+                continue;
+            }
+
+            if prev == num{
+                is_safe = false;
+                unsafe_count += 1;
+                //prev = num;     
+                if unsafe_count > 1{
+                    break;
+                }
+                continue;
+            }
+            let dis:i32 = (prev - num).abs();
+            if dis >= 1 && dis <= 3{
+                prev = num;
+            }else{
+                is_safe = false;
+                unsafe_count += 1;
+                //prev = num;
+                if unsafe_count > 1{
+                    break;
+                }
+                continue;
+            }
+        }
+        if unsafe_count == 0{
+            is_safe = true;
+        }
+        if is_safe{
+            safe += 1;
+        }
     }
 
-    left_numbers.sort();
-    right_numbers.sort();
-    
-    let ans: i32 = left_numbers
-        .iter()
-        .zip(right_numbers.iter())
-        .map(|(a,b)| (a-b).abs())
-        .sum();
+    println!("how many are safe: {}",safe);
 
-    println!("{}", ans);
 
     Ok(())
 } 
