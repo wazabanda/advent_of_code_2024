597242cafa29683441fbc44a561334885c14369a
wazabanda <1wazabanda@gmail.com>
2024-12-03 15:05:16 +0200

    day 3 part 1

--- src/main.rs
+++ src/main.rs
@@ -1,80 +1,40 @@
-use core::num;
 use std::fs::File;
 use std::io::{self,BufRead};
-
+use regex::Regex;
 //use std::path::Path;
 
 fn main() -> io::Result<()> {
     
-    let path = "src/inputs/day2.txt";
+    let path = "src/inputs/day3.txt";
     let file = File::open(path)?;
     let reader = io::BufReader::new(file);
 
-    let mut safe = 0;
-
-    let mut unsafe_logs:Vec<Vec<i32>>  = Vec::new();
-
+    let mut sum = 0;
     for line in reader.lines(){
         let line = line?;
-        let numbers: Vec<i32> = line
-            .split_whitespace()
-            .filter_map(|s| s.parse::<i32>().ok())
-            .collect();
-        let mut is_safe = true;
+        
+        let res = extract_mul_expressions(&line);
 
-        is_safe = process_line(&numbers);
-
-        if is_safe{
-            safe += 1;
-        }else{
-            unsafe_logs.push(numbers);
+        for (num1 , num2) in res{
+            sum += num1 * num2;
         }
     }
 
-    for number in unsafe_logs{
-        for i in 0..number.len(){
-            let mut number_removed = number.clone();
-            number_removed.remove(i);
-
-            if process_line(&number_removed){
-                safe += 1;
-                break;
-            }
-        }
-    }
-
-    println!("how many are safe: {}",safe);
-
+    println!("{}", sum);
 
     Ok(())
-} 
-
-fn is_sorted_ascending<T: Ord>(vec: &[T]) -> bool{
-    vec.windows(2).all(|w| w[0] <= w[1])
 }
 
-fn is_sorted_descending<T: Ord>(vec: &[T]) -> bool{
-    vec.windows(2).all(|w| w[0] >= w[1])
+
+fn extract_mul_expressions(input: &str) -> Vec<(i32,i32)> {
+
+    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+    re.captures_iter(input)
+        .map(|cap| {
+        let num1 = cap[1].parse::<i32>().unwrap();
+        let num2 = cap[2].parse::<i32>().unwrap();
+        (num1,num2)
+    })
+    .collect()
 }
 
-fn process_line(numbers:&Vec<i32>) -> bool
-{
-        // let mut prev = 0;
-        let mut is_safe = true;
-
-        let mut incr = is_sorted_ascending(&numbers);
-        
-        let mut unsafe_count = 0;
-
-        numbers.windows(2).all(|w|{
-            let prev = w[0];
-            let num = w[1];
-            let dis = (prev-num).abs();
-
-            if (incr && prev > num) || (incr == false && prev < num) || prev == num || dis < 1 || dis > 3{
-                return  false;
-            }
-            true
-        })
-        
-}
