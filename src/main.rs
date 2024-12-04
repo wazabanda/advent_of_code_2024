28a184dc5f1f2c62351474fce22848142be54887
wazabanda <1wazabanda@gmail.com>
2024-12-04 10:27:21 +0200

    doing day 4 part 2

--- src/main.rs
+++ src/main.rs
@@ -5,45 +5,156 @@ use regex::Regex;
 
 fn main() -> io::Result<()> {
     
-    let path = "src/inputs/day3.txt";
+    let path = "src/inputs/day4.txt";
     let file = File::open(path)?;
     let reader = io::BufReader::new(file);
 
     let mut sum = 0;
-    for line in reader.lines(){
-        let line = line?;
-        
-        for do_sub in line.split_terminator("do()"){
-            let parts: Vec<&str> = do_sub.split("don't()").collect();
-            if let Some(first_part) = parts.get(0) {
-                println!("{}",first_part);
-                let res = extract_mul_expressions(first_part);
-                
-                for (num1 , num2) in res{
-                    sum += num1 * num2;
-                }
-            
-            }
+    let mut sum2 = 0;
+    let lines: Vec<String> = reader.lines().filter_map(|line| line.ok()).collect();
+    let hor = lines[0].len();
+    let ver = lines.len();
 
+    for v in 0..ver{
+        let line = &lines[v];
+        sum += check_horizontal(&line);
+        let vert_slice = &lines[v..usize::min(v+4,lines.len())];
+        sum += check_vertical(vert_slice);
+        sum += check_diagonal(vert_slice,true,"XMAS");
+        sum += check_diagonal(vert_slice,false,"XMAS");
 
+        if v > 0 && v < ver -2{
+            let vert_slice = &lines[usize::min(v-1,line.len())..usize::min(v+2,lines.len())];
+            //println!("{:?}",vert_slice);
+            sum2 += check_diagonal_2(vert_slice);
         }
     }
 
-    println!("{}", sum);
+    println!("part 1: {}", sum);
+    println!("part 2: {}", sum2);
+
+
 
     Ok(())
 }
 
 
-fn extract_mul_expressions(input: &str) -> Vec<(i32,i32)> {
+fn check_horizontal(line: &String) -> i32{
+    
+    let mut count = 0;
+    for (index,cha) in line.chars().enumerate()
+    {
+        let end = usize::min(index+4,line.len());
+        let substring:String = (&line[index..end]).to_string();
+        let reversed:String = substring.chars().rev().collect();
+        if substring == "XMAS" || reversed == "XMAS"{
+            count += 1;
+        }
 
-    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
-    re.captures_iter(input)
-        .map(|cap| {
-        let num1 = cap[1].parse::<i32>().unwrap();
-        let num2 = cap[2].parse::<i32>().unwrap();
-        (num1,num2)
-    })
-    .collect()
+    }
+    count
+}
+
+fn check_str(res:&String,check: &str) -> (bool,bool)
+{
+    
+    if res == check{
+        return (true,true);
+    }
+
+    if res.chars().rev().collect::<String>() == check
+    {
+        return (true,false);
+    }   
+    (false,false)
+}
+
+fn check_diagonal_2(lines: &[String]) -> i32{
+    let hor = lines[0].len();
+    let mut count = 0;
+    for (index,c) in lines[1].chars().into_iter().enumerate() {
+        let mut res = String::new();
+        if c == 'A' && index > 0
+        {
+            if let Some(c) = lines[0].chars().nth(index-1){
+                res.push(c);
+            }
+            res.push('A');
+            if let Some(c) = lines[2].chars().nth(index+1){
+                res.push(c);
+            }
+            let first = check_str(&res,"MAS"); 
+
+            res.clear();
+            
+            if let Some(c) = lines[0].chars().nth(index+1){
+                res.push(c);
+            }
+            res.push('A');
+            if let Some(c) = lines[2].chars().nth(index-1){
+                res.push(c);
+            }
+            let second = check_str(&res,"MAS"); 
+            
+            if first.0 && second.0{
+                count += 1
+            }
+            
+            
+        }
+    
+    }
+    count
+}
+
+fn check_diagonal(lines: &[String], forward:bool, check: &str) -> i32{
+    let hor = lines[0].len();
+    let mut count = 0;
+    for i in 0..hor {
+        let mut res = String::new();
+        
+        for (index,line) in lines.into_iter().enumerate() {
+            let mut ind =  0 ;
+            if forward {ind = i + index;}
+            else{
+                if i < 3{
+                    break;
+                }else{
+                    ind = i - index;
+                }
+            }
+            if ind >= hor {
+                break;
+            }
+            if let Some(c) = line.chars().nth(ind) {
+                res.push(c);
+            }
+        }
+
+        if res == check || res.chars().rev().collect::<String>() == check{
+            count += 1;
+        }
+    }
+
+    count
+}
+
+fn check_vertical(lines: &[String]) -> i32 {
+    let mut count = 0;
+    let hor = lines[0].len();
+    for i in 0..hor {
+        let mut res = String::new();
+        
+        for line in lines.iter() {
+            if let Some(c) = line.chars().nth(i) {
+                res.push(c);
+            }
+        }
+
+        if res == "XMAS" || res.chars().rev().collect::<String>() == "XMAS"{
+            count += 1;
+        }
+    }
+    count
 }
 
